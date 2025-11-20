a1de2be216c406927b24dc18e472f4087e693484
Cl4Ud1O92 <155198363+Cl4Ud1O92@users.noreply.github.com>
2025-11-20 02:03:42 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
 
+app.get('/test', (req, res) => {
+  res.json({ message: 'Server backend attivo e funzionante!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend attivo su porta ${PORT}`);
+});
